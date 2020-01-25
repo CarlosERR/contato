640591c0ddc4b6099a12eb117e0d2d174005deb5
Carlos Ramos <kdu001987@gmail.com>
2020-01-25 17:28:23 -0300

    Formulário de contato

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+require __DIR__."/vendor/autoload.php";
+
+use Source\Support\Email;
+
+$email = new Email();
+
+$email->add(
+    "Olá Mundo é o Meu Primeiro E-mail!",
+    "<h1>Estou apenas testando!</h1>Espero que tenha dado certo!",
+    "Carlos Ramos",
+    "kdu001987@gmail.com",
+)->send();
+
+if(!$email->error()){
+    var_dump( true);
+}else{
+    echo $email->error()->getMessage();
+}
---
+++ source/Config.php
@@ -0,0 +1,20 @@
+<?php
+
+//HOST SMTP
+define("MAIL", [
+    "host" => "email-ssl.com.br",
+    "port" => "587",
+    "user" => "webdeveloper@conectario.com",
+    "passwd" => "Kdu*1987",
+    "from_name" => "Carlos Ramos",
+    "from_email" => "webdeveloper@conectario.com"
+]);
+
+define("MAIL_B", [
+    "host" => "",
+    "port" => "",
+    "user" => "",
+    "passwd" => "",
+    "from_name" => "Carlos Ramos",
+    "from_email" => "kdu001987@gmail.com"
+]);
